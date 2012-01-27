d4ec5dfa49baca33d037608f5b07249c1c09f643
neilduncan <neil@aqueduct.co.uk>
2012-01-27 14:12:33 +0000

    6

--- JenkinsTest/Program.cs
+++ JenkinsTest/Program.cs
@@ -8,7 +8,7 @@ namespace JenkinsTest
         static void Main(string[] args)
         {
             //test here 
-            Console.WriteLine("5");
+            Console.WriteLine("6");
         }
     }
 }
